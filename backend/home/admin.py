0eb1ac9f7d017dd240ccee25cc5b98be040cc01a
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-14 06:19:48 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Vbcd
+admin.site.register(Vbcd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+class Vbcd(models.Model):
+    'Generated Model'
+    cvbv = models.BigIntegerField()
+    sder = models.BigIntegerField()
+    def test_method(self):
+        import requests
+        response = requests.get("https://hello.com")
+        data = response.json()
+        return data
